93419599227692d90d669b7df958fdbfe3d5e600
Aitalina <ms.aitam@mail.ru>
2023-11-16 10:51:51 +0600

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.js';
+import path from 'path';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format');
-
-program.command('gendiff');
-
-program.parse(process.argv);
-
-if (program.help) {
-  console.log(program.outputHelp());
-}
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    genDiff(filepath1, filepath2);
+  })
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .parse(process.argv);

17c3d3e3568ba8232633d7b049fa3ee95ea188b5
Aitalina <ms.aitam@mail.ru>
2023-12-07 15:41:02 +0600

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,74 +1,43 @@
-import { readFileSync } from 'node:fs';
-import yaml from 'js-yaml';
-import path from 'path';
 import _ from 'lodash';
+import path from 'path';
+import buildCart from './parsers.js';
+import formatter from '../formatters/index.js';
 
-const jsonParse = (pathToFile) => {
-  const data = readFileSync(pathToFile, 'utf8');
-  const jsonData = JSON.parse(data);
-  return jsonData;
+const getAbsolutePath = (filepath) => {
+  const absPath = filepath[0] !== '/' ? path.resolve(process.cwd(), filepath) : filepath;
+  return absPath;
 };
 
-const yamlParse = (pathToFile) => {
-  const data = readFileSync(pathToFile, 'utf8');
-  const yamlData = yaml.safeLoad(data);
-  return yamlData;
-};
-
-const getOutput = (file1, file2) => {
-  const combinedKeys = { ...file1, ...file2 };
-  const uniqueKeys = _.sortBy(Object.keys(combinedKeys));
-  const result = uniqueKeys.reduce((acc, key) => {
-    let newAcc = acc;
-    if (file1.hasOwnProperty(key)) {
-      if (file2.hasOwnProperty(key)) {
-        if (file1[key] === file2[key]) {
-          newAcc += `${' '.repeat(4)}${key}: ${file1[key]}\n`;
-        } else {
-          newAcc += `  - ${key}: ${file1[key]}\n`;
-          newAcc += `  + ${key}: ${file2[key]}\n`;
-        }
-      } else {
-        newAcc += `  - ${key}: ${file1[key]}\n`;
-      }
+const getDiff = (file1, file2) => {
+  const mergedKeys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
+  const result = mergedKeys.reduce((acc, key) => {
+    const value1 = file1[key];
+    const value2 = file2[key];
+    if (_.isEqual(value1, value2)) {
+      acc[key] = { status: 'unchanged', value: value1 };
+    } else if (!_.has(file1, key)) {
+      acc[key] = { status: 'added', value: value2 };
+    } else if (!_.has(file2, key)) {
+      acc[key] = { status: 'deleted', value: value1 };
+    } else if (_.isObject(value1) && _.isObject(value2)) {
+      acc[key] = { status: 'unchanged', children: getDiff(value1, value2) };
     } else {
-      newAcc += `  + ${key}: ${file2[key]}\n`;
+      acc[key] = { status: 'changed', oldValue: value1, newValue: value2 };
     }
-    return newAcc;
-  }, '');
-  return `{\n${result}}`;
-};
-
-const parse = (firstFile, secondFile) => {
-  const fileExtension1 = path.extname(firstFile);
-  const fileExtension2 = path.extname(secondFile);
-  let data1;
-  let data2;
-  if (fileExtension1 === '.json') {
-    data1 = jsonParse(firstFile);
-  } else if (fileExtension1 === '.yml') {
-    data1 = yamlParse(firstFile);
-  }
-  if (fileExtension2 === '.json') {
-    data2 = jsonParse(secondFile);
-  } else if (fileExtension2 === '.yml') {
-    data2 = yamlParse(secondFile);
-  }
-  const result = getOutput(data1, data2);
+    return acc;
+  }, {});
   return result;
 };
 
-const genDiff = (filepath1, filepath2) => {
-  let absolutePath1 = filepath1;
-  let absolutePath2 = filepath2;
-  if (filepath1[0] !== '/') {
-    absolutePath1 = path.resolve(process.cwd(), filepath1);
-  }
-  if (filepath2[0] !== '/') {
-    absolutePath2 = path.resolve(process.cwd(), filepath2);
-  }
-  const result = parse(absolutePath1, absolutePath2);
-  return result;
+const genDiff = (filepath1, filepath2, formatName) => {
+  const absolutePath1 = getAbsolutePath(filepath1);
+  const absolutePath2 = getAbsolutePath(filepath2);
+
+  const data1 = buildCart(absolutePath1);
+  const data2 = buildCart(absolutePath2);
+  const diff = getDiff(data1, data2);
+  const outputString = formatter(formatName, diff);
+  return outputString;
 };
 
 export default genDiff;

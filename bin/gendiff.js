7b2325de0e2b4b2f8d3b3f4faf39e636049d28ac
Aitalina <ms.aitam@mail.ru>
2023-12-07 15:36:06 +0600

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 import genDiff from '../src/index.js';
-import path from 'path';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, options) => {
+    const formatName = options.format;
+    console.log(genDiff(filepath1, filepath2, formatName));
   })
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format (default: "stylish")', 'stylish')
   .parse(process.argv);

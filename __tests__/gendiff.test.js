521f9fa3cf3257ee6af144659985fc7c9a75217d
Aitalina <ms.aitam@mail.ru>
2023-12-11 12:20:15 +0600

    add json test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,12 +8,13 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('test1', () => {
-  const filePath1 = getFixturePath('file1.json');
-  const filePath2 = getFixturePath('file2.json');
-  const filePath3 = getFixturePath('file1.yml');
-  const filePath4 = getFixturePath('file2.yml');
-  const resultStylish = `{
+const filePath1 = getFixturePath('file1.json');
+const filePath2 = getFixturePath('file2.json');
+const filePath3 = getFixturePath('file1.yml');
+const filePath4 = getFixturePath('file2.yml');
+
+test('testStylish', () => {
+  const result = `{
     common: {
       + follow: false
         setting1: Value 1
@@ -57,7 +58,11 @@ test('test1', () => {
         fee: 100500
     }
 }`;
-  const resultPlain = `Property 'common.follow' was added with value: false
+  expect(genDiff(filePath1, filePath4, 'stylish')).toBe(result);
+});
+
+test('testPlain', () => {
+  const result = `Property 'common.follow' was added with value: false
 Property 'common.setting2' was removed
 Property 'common.setting3' was updated. From true to null
 Property 'common.setting4' was added with value: 'blah blah'
@@ -68,6 +73,22 @@ Property 'group1.baz' was updated. From 'bas' to 'bars'
 Property 'group1.nest' was updated. From [complex value] to 'str'
 Property 'group2' was removed
 Property 'group3' was added with value: [complex value]`;
-  expect(genDiff(filePath1, filePath4, 'stylish')).toBe(resultStylish);
-  expect(genDiff(filePath3, filePath2, 'plain')).toBe(resultPlain);
+  expect(genDiff(filePath3, filePath2, 'plain')).toBe(result);
+});
+
+test('testJson', () => {
+  const result = [
+    { op: 'add', path: '/common/follow', value: false },
+    { op: 'remove', path: '/common/setting2' },
+    { op: 'replace', path: '/common/setting3', value: null },
+    { op: 'add', path: '/common/setting4', value: 'blah blah' },
+    { op: 'add', path: '/common/setting5', value: { key5: 'value5' } },
+    { op: 'replace', path: '/common/setting6/doge/wow', value: 'so much' },
+    { op: 'add', path: '/common/setting6/ops', value: 'vops' },
+    { op: 'replace', path: '/group1/baz', value: 'bars' },
+    { op: 'replace', path: '/group1/nest', value: 'str' },
+    { op: 'remove', path: '/group2' },
+    { op: 'add', path: '/group3', value: { deep: { id: { number: 45 } }, fee: 100500 } },
+  ];
+  expect(genDiff(filePath3, filePath4, 'json')).toStrictEqual(result);
 });

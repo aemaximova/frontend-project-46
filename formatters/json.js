6ea3523a99f35a105156f6c0a8d7fb611a1732c8
Aitalina <ms.aitam@mail.ru>
2023-12-18 13:33:08 +0600

    bug fixed

--- formatters/json.js
+++ formatters/json.js
@@ -1,32 +1,35 @@
 import _ from 'lodash';
 
-const json = (diff, path = '') => {
-  const lines = Object
-    .entries(diff)
-    .flatMap(([key, value]) => {
-      const currentPath = `${path}/${key}`;
-      const {
-        status, children, newValue, value: singleValue,
-      } = value;
+const json = (data, path = '') => {
+  const iter = (diff) => {
+    const lines = Object
+      .entries(diff)
+      .flatMap(([key, value]) => {
+        const currentPath = `${path}/${key}`;
+        const {
+          status, children, newValue, value: singleValue,
+        } = value;
 
-      if (children) {
-        return json(children, currentPath);
-      }
-      if (_.has(value, 'value') || _.has(value, 'oldValue')) {
-        switch (status) {
-          case 'changed':
-            return JSON.stringify({ op: 'replace', path: `${path}/${key}`, value: newValue });
-          case 'deleted':
-            return JSON.stringify({ op: 'remove', path: `${path}/${key}` });
-          case 'added':
-            return JSON.stringify({ op: 'add', path: `${path}/${key}`, value: singleValue });
-          default:
-            return [];
+        if (children) {
+          return iter(children, currentPath);
         }
-      }
-      return [];
-    });
-  return `[${lines.join('\n')}]`;
+        if (_.has(value, 'value') || _.has(value, 'oldValue')) {
+          switch (status) {
+            case 'changed':
+              return JSON.stringify({ op: 'replace', path: `${path}/${key}`, value: newValue });
+            case 'deleted':
+              return JSON.stringify({ op: 'remove', path: `${path}/${key}` });
+            case 'added':
+              return JSON.stringify({ op: 'add', path: `${path}/${key}`, value: singleValue });
+            default:
+              return [];
+          }
+        }
+        return [];
+      });
+    return lines.join('');
+  };
+  return `[${iter(data)}]`;
 };
 
 export default json;
